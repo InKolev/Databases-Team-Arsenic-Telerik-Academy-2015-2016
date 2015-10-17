218861db08e26b156504db365a82e903aec6aec9
deyantodorov <deyan.todorov@hotmail.com>
2015-10-18 02:09:54 +0300

    Save Products to MongoDb

--- Solution/Toys.ConsoleClient/StartUp.cs
+++ Solution/Toys.ConsoleClient/StartUp.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using Toys.Core.MongoDb;
     using Toys.Data;
 
     public class StartUp
@@ -13,6 +14,11 @@
             var db = new ToysData(context);
 
             Console.WriteLine(db.Products.All().Count());
+
+            var mongo = new MongoDbOperations();
+            //mongo.ImportProducts();
+            mongo.LoadProducts();
+
         }
     }
 }
